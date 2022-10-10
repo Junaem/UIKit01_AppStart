f807a2688b1333ef90130854312cd016d0213061
junaem <junh9547@gmail.com>
2022-10-11 02:36:24 +0900

    OnBoardingView Start

---
+++ UIKit06_OnBoardingView/UIKit06_OnBoardingView/OnBoardingItemViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OnBoardingItemViewController.swift
+//  UIKit06_OnBoardingView
+//
+//  Created by 이준형 on 2022/10/11.
+//
+
+import UIKit
+
+class OnBoardingItemViewController: UIViewController {
+    
+    @IBOutlet weak var topImageView: UIImageView!
+    @IBOutlet weak var mainTitleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ UIKit06_OnBoardingView/UIKit06_OnBoardingView/OnBoardingPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnBoardingPageViewController.swift
+//  UIKit06_OnBoardingView
+//
+//  Created by 이준형 on 2022/10/11.
+//
+
+import UIKit
+
+class OnBoardingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
