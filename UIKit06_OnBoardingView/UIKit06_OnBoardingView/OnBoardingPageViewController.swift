064ece171fd418ef7d51d5f51937d1ceb8d1468b
junaem <junh9547@gmail.com>
2022-10-16 02:10:38 +0900

    OnBoardingView Done!

--- UIKit06_OnBoardingView/UIKit06_OnBoardingView/OnBoardingPageViewController.swift
+++ UIKit06_OnBoardingView/UIKit06_OnBoardingView/OnBoardingPageViewController.swift
@@ -11,6 +11,13 @@ class OnBoardingPageViewController: UIPageViewController {
     
     var pages = [UIViewController]()
     var bottomButtonMargin: NSLayoutConstraint?
+    var pageControl = UIPageControl()
+//    let startIndex = 0
+    var currentIndex = 0 {
+        didSet {
+            pageControl.currentPage = currentIndex
+        }
+    }
     
     func makePageVC() {
         let itemVC1 = OnBoardingItemViewController(nibName: "OnBoardingItemViewController", bundle: nil)
@@ -42,26 +49,52 @@ class OnBoardingPageViewController: UIPageViewController {
         
         makePageVC()
         makeBottomButton()
-        
-        func makeBottomButton() {
-            let button = UIButton()
-            button.setTitle("확인", for: .normal)
-            button.setTitleColor(.black, for: .normal)
-            button.backgroundColor = .systemBlue
-            button.addTarget(self, action: #selector(dismissPageVC), for: .touchUpInside)
+        makePageControl()
+    }
+    func makeBottomButton() {
+        let button = UIButton()
+        button.setTitle("확인", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(dismissPageVC), for: .touchUpInside)
 //            button.titleLabel?.font = .systemFont(ofSize: 20)
-            self.view.addSubview(button)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            
-            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true // contant를 0으로 줄 때는 생략 가능
-            button.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-            button.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-            button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-            
-            bottomButtonMargin = button.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
-            bottomButtonMargin?.isActive = true
-            hideButton()
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true // contant를 0으로 줄 때는 생략 가능
+        button.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        button.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        bottomButtonMargin = button.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        bottomButtonMargin?.isActive = true
+        hideButton()
+    }
+    
+    
+    func makePageControl() {
+        self.view.addSubview(pageControl)
+        pageControl.translatesAutoresizingMaskIntoConstraints = false   // 오토레이아웃 쓰겠다.
+        pageControl.currentPageIndicatorTintColor = .darkGray
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = currentIndex    // 이 값은 전역변수 currentIndex의 didSet으로 계속 변화함
+//        pageControl.isUserInteractionEnabled = false    // 터치로 조작 불가하고 보여주기만 하고 싶을때
+        
+        pageControl.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -80).isActive = true
+        pageControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        pageControl.addTarget(self, action: #selector(pageControlTapped), for: .valueChanged)
+    }
+    
+    @objc func pageControlTapped(sender: UIPageControl) {
+        if sender.currentPage > self.currentIndex {
+            setViewControllers([pages[sender.currentPage]], direction: .forward, animated: true, completion: nil)
+        } else {
+            setViewControllers([pages[sender.currentPage]], direction: .reverse, animated: true, completion: nil)
         }
+        currentIndex = sender.currentPage
+        buttonPresent()
     }
     
     @objc func dismissPageVC() {
@@ -76,6 +109,8 @@ extension OnBoardingPageViewController: UIPageViewControllerDataSource{
         guard let currentIndex = pages.firstIndex(of: viewController) else {
             return nil // guard else를 사용해, 인덱스를 못 찾으면 그냥 nil을 리턴
         }
+        self.currentIndex = currentIndex    //  pageControl에 사용 - delegate에서 써서 안써도 되는듯
+        
         if currentIndex == 0 {
             return pages.last
         } else {
@@ -87,6 +122,8 @@ extension OnBoardingPageViewController: UIPageViewControllerDataSource{
         guard let currentIndex = pages.firstIndex(of: viewController) else {
             return nil // guard를 사용해, 인덱스를 못 찾으면 그냥 nil을 리턴
         }
+        self.currentIndex = currentIndex
+        
         if currentIndex == pages.count - 1 {
             return pages.first
         } else {
@@ -98,7 +135,6 @@ extension OnBoardingPageViewController: UIPageViewControllerDataSource{
 
 extension OnBoardingPageViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        print("finsh")
 
         guard let currentVC = pageViewController.viewControllers?.first else {
             return
@@ -107,21 +143,33 @@ extension OnBoardingPageViewController: UIPageViewControllerDelegate {
         guard let currentIndex = pages.firstIndex(of: currentVC) else {
             return
         }
-
+        
+        self.currentIndex = currentIndex
+        buttonPresent()
+    }
+    
+    func buttonPresent() {
         if currentIndex == pages.count - 1 {
             self.showButton()
         } else {
             self.hideButton()
         }
-
+        
+        // 밑에 거랑 같은 코드인데 completion으로 포지션 건드릴 필요가 없을 때 사용(밑에도 안 쓰긴 함)
+        // self.view.layoutIfNeeded()로 현재 뷰에 화면 변화가 필요하면 애니메이션으로 보여줌(위에서 show/hideButton으로 화면 변화 됐을때)
+//        UIView.animate(withDuration: 0.5) {
+//            self.view.layoutIfNeeded()
+//        }
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: [.curveEaseInOut], animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
     func showButton() {
         bottomButtonMargin?.constant = 0
-        print("show")
     }
     func hideButton() {
         bottomButtonMargin?.constant = 100
-        print("hide")
     }
 }
--- UIKit06_OnBoardingView/UIKit06_OnBoardingView/ViewController.swift
+++ UIKit06_OnBoardingView/UIKit06_OnBoardingView/ViewController.swift
@@ -8,18 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var didShowOnboardingView = false
+    let onboardingViewButton = UIButton()   // 이거는 그냥 추가로 한 번 만들어봄
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        makeOnboardingViewButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
     }
 
     override func viewDidAppear(_ animated: Bool) {
+        if !didShowOnboardingView {
+            showOnboardingView()
+            didShowOnboardingView = true
+        }
+    }
+    
+    func makeOnboardingViewButton() {
+        onboardingViewButton.setTitle("온보딩 보기", for: .normal)
+        onboardingViewButton.setTitleColor(.black, for: .normal)
+        onboardingViewButton.backgroundColor = .systemBlue
+        onboardingViewButton.addTarget(self, action: #selector(showOnboardingView), for: .touchUpInside)
+        self.view.addSubview(onboardingViewButton)
+        
+        onboardingViewButton.translatesAutoresizingMaskIntoConstraints = false
+        onboardingViewButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
+        onboardingViewButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        onboardingViewButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        onboardingViewButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    @objc func showOnboardingView() {
         let pageVC = OnBoardingPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: .none)
         pageVC.modalPresentationStyle = .fullScreen
         self.present(pageVC, animated: true, completion: nil)
-        
     }
 }
 
