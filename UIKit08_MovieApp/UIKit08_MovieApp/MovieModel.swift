34f28ea09a68143eae81993b0c8756084860cb55
junaem <junh9547@gmail.com>
2022-11-09 16:58:38 +0900

    MovieApp 03

---
+++ UIKit08_MovieApp/UIKit08_MovieApp/MovieModel.swift
@@ -0,0 +1,36 @@
+//
+//  MovieModel.swift
+//  UIKit08_MovieApp
+//
+//  Created by 이준형 on 2022/11/09.
+//
+
+import Foundation
+
+struct MovieModel: Codable {    // Json 바로 인코딩, 디코딩 가능하게 해주는 프로토콜
+    let resultCount: Int
+    let results: [Result]
+    
+}
+
+struct Result: Codable {
+    let trackName: String  // api에서 없을 수도 있으니 Optional
+    let previewUrl: String
+    let image: String
+    let shortDescription: String?
+    let longDescription: String
+    let currency: String
+    let trackPrice: Double
+//    let releaseDate: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case image = "artworkUrl100"    // 인코딩했을때는 aU100, 디코딩했을때는 image
+        case trackName  // 같은 이름 그대로 쓸거면 뒤에 값은 안 넣어도 됨
+        case previewUrl
+        case shortDescription
+        case longDescription
+        case currency
+        case trackPrice
+//        case releaseDate
+    }
+}
--- UIKit08_MovieApp/UIKit08_MovieApp/ViewController.swift
+++ UIKit08_MovieApp/UIKit08_MovieApp/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var movieModel: MovieModel?
+    
     @IBOutlet weak var movieTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -20,21 +22,62 @@ class ViewController: UIViewController {
         movieTableView.dataSource = self
         searchBar.delegate = self
         
-        
-        
-        
+        requestMovieApi()
     }
 
+    func requestMovieApi() {
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        var components = URLComponents(string: "https://itunes.apple.com/search")
+        
+        let term = URLQueryItem(name: "term", value: "marvel")
+        let media = URLQueryItem(name: "media", value: "movie")
+        
+        components?.queryItems = [term, media]
+        guard let url = components?.url else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            print( (response as! HTTPURLResponse).statusCode )
+            
+            if let hasData = data {
+                do {
+                    self.movieModel = try JSONDecoder().decode(MovieModel.self, from: hasData)
+                    print(self.movieModel ?? "No data")
+                    DispatchQueue.main.async {
+                        self.movieTableView.reloadData()    // 클로져 안이기 때문에 매인 스레드에서 갱신
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        
+        }
+        task.resume()
+        session.finishTasksAndInvalidate()
+        
+    }
 
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.movieModel?.resultCount ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        cell.titleLabel.text = self.movieModel?.results[indexPath.row].trackName
+        cell.descriptionLabel.text = self.movieModel?.results[indexPath.row].shortDescription
+        
+        let currency = self.movieModel?.results[indexPath.row].currency ?? ""
+        let price = self.movieModel?.results[indexPath.row].trackPrice.description ?? ""
+        cell.priceLabel.text = currency + price
         return cell
     }
     
@@ -43,7 +86,6 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.text
     }
     
 }
